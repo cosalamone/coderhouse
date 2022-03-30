22550b23dcdd69d4d1cdac7b16995930d9b33062
cosalamone <cosalamone@gmail.com>
2022-03-30 17:59:32 -0300

    Actividad de clase 1, 29-03-2022

---
+++ clase-1-actividad-1.js
@@ -0,0 +1,6 @@
+alert("¡Bienvenido a la primera actividad!")
+
+let nombre = prompt("¿Cuál es tu nombre?");
+let apellido = prompt("¿Cuál es tu apellido?");
+
+alert("Hola " + nombre + " " + apellido);
