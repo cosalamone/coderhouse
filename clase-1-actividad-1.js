96452058d4ce03c77ef93fafdb704b1d60c57d4d
cosalamone <cosalamone@gmail.com>
2022-03-30 18:40:58 -0300

    Agregado practica prompt con suma de numeros y parseInt()

--- clase-1-actividad-1.js
+++ clase-1-actividad-1.js
@@ -1,6 +1,12 @@
-alert("¡Bienvenido a la primera actividad!")
+alert("¡Bienvenido a la primera actividad!");
 
 let nombre = prompt("¿Cuál es tu nombre?");
 let apellido = prompt("¿Cuál es tu apellido?");
 
 alert("Hola " + nombre + " " + apellido);
+
+let edad = prompt ("¿Cuantos años tenes?");
+
+alert("Gracias por responder. ¿Que datos consegu+i? Ahora se que " + nombre + " tiene " + edad + " años")
+
+alert("Si " + nombre + " ahora tiene " + edad + " años, dentro de 5 años tendrá " + (parseInt(edad) + 5))
