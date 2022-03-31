d7eb88da9c0b52e3e18b42035b58831fbbcfe033
cosalamone <cosalamone@gmail.com>
2022-03-31 18:55:58 -0300

    Actividad 5 modificada

--- actividades-booklet-1/actividad-5-descuentos.js
+++ actividades-booklet-1/actividad-5-descuentos.js
@@ -4,6 +4,9 @@ valores ingresados, y realizar una salida.
 */
 
 let precio1=prompt("Por favor, indicanos el precio de tu producto al cual debemos aplicarle el 30% OFF")
+let precio2=prompt("Ahora indicanos el precio para aplicarle un 45%OFF")
 let descuento30off= parseInt(precio1)*0.7;
+let descuento45off=parseInt(precio2)*0.55;
 
 alert("Tu producto con el descuento queda en $"+descuento30off);
+alert("Tu segundo producto queda en $"+descuento45off);
