3ad41690419b70f15fcda460e1c0f41f8097b647
cosalamone <cosalamone@gmail.com>
2022-04-23 20:58:58 -0300

    agregado de funcion carrito de compras

--- proyecto-preentregas/tienda-online.js
+++ proyecto-preentregas/tienda-online.js
@@ -1,3 +1,4 @@
+
 // CLASES PLANTAS Y MACETAS
 class Planta {
     constructor(categoria, especie, tipo, precio, foto, descripcion,) {
@@ -13,6 +14,7 @@ class Planta {
     }
 }
 
+
 class Maceta {
     constructor(categoria, material, color, tamaño, foto, precio, descripcion) {
         this.categoria = categoria;
@@ -27,6 +29,7 @@ class Maceta {
         return this.categoria + " de " + this.material + " N" + this.tamaño
     }
 }
+// FIN CLASES PLANTAS Y MACETAS
 
 
 
@@ -50,38 +53,126 @@ let listaProductos = [
     new Maceta("macetas", "barro", "terracota", 12, "./fotos/rsz_maceta-barro-12.jpg", 120, "Maceta de barro. Tamaño numero 12"),
     new Maceta("macetas", "barro", "terracota", 24, "./fotos/rsz_maceta-barro-24.jpg", 200, "Maceta de barro. Tamaño numero 24"),
 ]
+// PRODUCTOS 
 
 let html = "";
 
 
-window.onload = mostrarProductos(listaProductos);
+window.onload = mostrarProductos(listaProductos); //UBICACION PARA QUE FUNCIONE, DESPUES DE LOS LET 
 
 
+// FUNCION MOSTRARPRODUCTOS
 function mostrarProductos(listaProductos) {
-
+    
     for (let i = 0; i < listaProductos.length; i++) {
         document.getElementById("articulos").innerHTML += generarHTML(listaProductos[i])
     }
 
 }
+// FUNCION MOSTRARPRODUCTOS
 
-
+// FUNCION GENERARHTML
 function generarHTML(producto) {
     html =
-        `<div class="col">
-        <div class="card">
-             <div title= "${producto.titulo()}" class="cover cover-small"
-                style="background-image: url(${producto.foto})" ;>
-            </div>
-
-             <div class="card-body">
-                 <h5 class="card-title">${producto.titulo()}</h5>
-                 <p>${producto.descripcion}</p>
-                 <span class="badge bg-dark">$${producto.precio}</span>
-
-                 </div>
-            </div >
+    `<div class="col">
+    <div class="card">
+    <div title= "${producto.titulo()}" class="cover cover-small"
+    style="background-image: url(${producto.foto})" ;>
+    </div>
+    
+    <div class="card-body">
+    <h5 class="card-title">${producto.titulo()}</h5>
+    <p class="card-text">${producto.descripcion}</p>
+    <span class="badge bg-dark">$${producto.precio}</span>
+    
+    </div>
+    </div >
     </div > `
-
+    
     return html
 }
+// FUNCION GENERARHTML
+
+
+
+
+
+
+
+window.onload = carritoCompra();
+
+// FUNCION PRINCIPAL CARRITO COMPRA
+
+function carritoCompra() {
+    let compra = lista();
+    calcularCosto(compra);
+}
+// FIN FUNCION PRINCIPAL
+
+
+// funcion accesoria 1 - obtener la lista de compra 
+function lista() {
+    let compra = [];
+    let promptIngresado = "";
+
+    do {
+        promptIngresado = prompt("¿Qué plantas querés comprar? Al finalizar su carrito escriba 'fin'").toLowerCase();
+
+        if (promptIngresado != "fin")
+            compra.push(promptIngresado);
+
+    }
+    while (promptIngresado != "fin")
+
+    return (compra);
+}
+// FIN funcion accesoria 1 - obtener la lista de compra 
+
+
+
+// funcion accesoria 2 - calcular costo de lista de compra 
+
+function calcularCosto(compra) {
+
+    let costo = 0;
+    let sinStock = [];
+    for (let i = 0; i < compra.length; i++) {
+
+        let precio = precioProducto(compra[i]);
+        ;
+
+        if (precio != null) {
+
+            costo = costo + precio;
+
+        } else {
+            sinStock.push(compra[i]);
+        }
+
+    }
+    if (sinStock.length > 0) {
+        alert("El/los producto/s " + sinStock + " no se agregaron al carrito de compras porque no contamos con stock");
+
+    }
+
+    alert("El valor total de tu compra incluyendo el envío de $349 es de " + (costo + 349));
+
+}
+//  FIN funcion accesoria 2 - calcular costo de lista de compra 
+
+
+
+// funcion 3 - calcular Precio del Producto
+
+function precioProducto(nombreProducto) {
+
+    for (let i = 0; i < listaProductos.length; i++) {
+        if (nombreProducto == listaProductos[i].especie) {
+            return (listaProductos[i].precio);
+        }
+
+    }
+    return (null);
+}
+
+// FIN funcion 3 - calcular Precio del Producto
